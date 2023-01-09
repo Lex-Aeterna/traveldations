59786a9ecd4ab39719434e45d4f8c503c1848f58
Alex Barron <aleksandra.barron@boundless.com>
2023-01-08 22:25:15 -0500

    Continue working on routes

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -16,8 +16,33 @@ class DestinationsController < ApplicationController
     #whitelisting the params
     @destination = Destination.new(params.require(:destination).permit(:country, :city, :description))
     #save to db
-    @destination.save
-    redirect_to destination_path(@destination)
+    if @destination.save
+      flash[:notice] = 'Destination saved succesfully'
+      redirect_to destination_path(@destination)
+    else
+      render :new
+    end
   end
 
+  def edit
+    @destination = Destination.find(params[:id])
+  end
+
+  def update
+    @destination = Destination.find(params[:id])
+    if @destination.update(params.require(:destination).permit(:country, :city, :description))
+      flash[:notice] = 'Destination was updated sucessfully'
+      redirect_to @destination
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @destination = Destination.find(params[:id])
+    @destination.destroy
+    flash[:notice] = 'Destination was deleted successfully'
+    redirect_to destination_path
+  end
+  
 end
