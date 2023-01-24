dba7f87dff5e15c35fdfa2b6921283cda638ef2d
Alex Barron <aleksandra.barron@boundless.com>
2023-01-23 20:21:55 -0500

    Add Activr Storage Images functionality

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,5 +1,9 @@
 class Destination < ApplicationRecord
-  validates :city, :country, :description, presence: true
+  validates :city, :country, :description, :image, presence: true
   validates :city, :country, length: { in: 2..100 }
   validates :description, length: { in: 3..300 }
+
+  has_one_attached :image do |attachable|
+      attachable.variant :thumb, resize_to_limit: [300,300]
+  end
 end
